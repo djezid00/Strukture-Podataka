016a7ea61ea092403cf1833373865e7c7c9bf974
djezid00 <92431473+djezid00@users.noreply.github.com>
2022-09-07 17:16:29 +0200

    Update Zadatak 8.c

--- Zadatak 8.c
+++ Zadatak 8.c
@@ -1,288 +1,311 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#define MAX (10)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct stablo* Stablo;
-typedef struct stablo {
+struct _stablo;
+typedef struct _stablo* Stablo;
+typedef struct _stablo {
 	int el;
-	Stablo lijevo;
-	Stablo desno;
-};
+	Stablo L;
+	Stablo D;
+}Tree;
 
-Stablo unos(Stablo);
-Stablo noviEl(Stablo, int);
-int broj();
 int Izbornik();
-int inorder(Stablo);
-int preorder(Stablo);
-int postorder(Stablo);
-int PrintLevelorder(Stablo);
-int PrintLevel(Stablo, int);
-int Height(Stablo);
-Stablo findElement(Stablo, int);
-Stablo findMinElement(Stablo);
-Stablo izbrisiElement(const Stablo, Stablo);
+Stablo Izbor(Stablo root);
+int broj();
+Stablo noviEl(Stablo root,int broj);
+int Inorder(Stablo root);
+int Preorder(Stablo root);
+int Postorder(Stablo root);
+int LevelOrder(Stablo root);
+int Height(Stablo root);
+int PrintLevel(Stablo root, int level);
+Stablo findElement(Stablo root, int broj);
+Stablo DeleteNode(Stablo root,Stablo izbor);
+Stablo FindMin(Stablo root);
+Stablo DealocirajStablo(Stablo root);
+
 
 
 int main()
 {
-	Stablo root;
-	root = NULL;
+	Stablo root = NULL;
 
 	Izbornik();
 
 	do
 	{
-		root = unos(root);
+		root = Izbor(root);
 	} while (root);
 
-	return 0;
-}
 
-
-
-Stablo noviEl(Stablo root, int Broj)
-{
-	Stablo novi = (Stablo)malloc(sizeof(Stablo));
-	novi = root;
-	if (novi == NULL) {
-		Stablo novi = (Stablo)malloc(sizeof(Stablo));
-		if (!novi)
-		{
-			perror("Ne mogu alocirati memoriju!\n");
-			return NULL;
-		}
-		novi->el = Broj;
-		novi->lijevo = NULL;
-		novi->desno = NULL;
-
-		return novi;
-	}
-
-	else if (Broj > novi->el)
-		novi->desno = noviEl(novi->desno, Broj);
-
-	else if (Broj < novi->el)
-		novi->lijevo = noviEl(novi->lijevo, Broj);
-
-	return novi;
-}
-
-Stablo unos(Stablo root)
-{
-	int odabir = 0;
-	Stablo izbor = NULL;
-
-	printf("\nUnos: ");
-	scanf("%d", &odabir);
-
-	switch (odabir) {
-	case 1:
-		root = noviEl(root, broj());
-		return root;
-	case 2:
-		printf("\nINORDER:");
-		inorder(root);
-		printf("\n");
-		return root;
-
-	case 3:
-		printf("\nPREORDER:");
-		preorder(root);
-		printf("\n");
-		return root;
-
-	case 4:
-		printf("\nPOSTORDER:");
-		postorder(root);
-		printf("\n");
-		return root;
-
-	case 5:
-		printf("\nLEVEL ORDER:");
-		PrintLevelorder(root);
-		printf("\n");
-		return root;
-
-	case 6:
-		izbor = findElement(root, broj());
-		izbrisiElement(root, izbor);
-		return root;
-
-
-	default:
-		printf("Pogresan unos!\n");
-		return 1;
-	}
-
-}
-
-int broj()
-{
-	int br = 0;
-	printf("Unesi broj: ");
-	scanf("%d", &br);
-
-	return br;
-}
-
-
-int preorder(Stablo root)
-{
-	if (root == NULL)
-		return NULL;
-
-	printf(" %d ", root->el);
-	preorder(root->lijevo);
-	preorder(root->desno);
 
 	return EXIT_SUCCESS;
-}
-
-int inorder(Stablo root)
-{
-	if (root == NULL)
-		return NULL;
-	inorder(root->lijevo);
-	printf(" %d ", root->el);
-	inorder(root->desno);
-
-	return EXIT_SUCCESS;
-}
-int postorder(Stablo root)
-{
-	if (root == NULL)
-		return NULL;
-	postorder(root->lijevo);
-	postorder(root->desno);
-	printf(" %d ", root->el);
-
-	return EXIT_SUCCESS;
-}
-int PrintLevelorder(Stablo Root)
-{
-	int h = Height(Root);
-	int i = 1;
-
-	for (i = 1; i <= h; i++)
-		PrintLevel(Root, i);
-
-	return EXIT_SUCCESS;
-}
-
-int PrintLevel(Stablo Root, int level)
-{
-	if (Root == NULL)
-		return EXIT_SUCCESS;
-
-	if (level == 1)
-		printf(" %d ", Root->el);
-
-	else if (level > 1)
-	{
-		PrintLevel(Root->lijevo, level - 1);
-		PrintLevel(Root->desno, level - 1);
-	}
-
-	return EXIT_SUCCESS;
-}
-
-int Height(Stablo Root)
-{
-	if (Root == NULL)
-		return 0;
-
-	else
-	{
-		int lheight = Height(Root->lijevo);
-		int rheight = Height(Root->desno);
-
-		if (lheight > rheight)
-			return(lheight + 1);
-
-		else
-			return(rheight + 1);
-	}
-}
-
-
-
-Stablo findElement(Stablo root, int p)
-{
-	Stablo element = NULL;
-	element = root;
-
-	if (element == NULL)
-		return NULL;
-
-	else if (element->el == p)
-		return element;
-
-	else if (p > element->el)
-		return findElement(element->desno, p);
-
-	else if (p < element->el)
-		return findElement(element->lijevo, p);
-
-	return element;
-}
-
-Stablo findMinElement(Stablo root)
-{
-	if (root == NULL)
-		return NULL;
-	else if (root->lijevo == NULL)
-		return root;
-
-	return findMinElement(root->lijevo);
-}
-
-Stablo izbrisiElement(const Stablo root, Stablo obrisi)
-{
-	Stablo pom = (Stablo)root;
-	Stablo temp = NULL;
-
-	if (pom == NULL)
-		return NULL;
-
-	else if (obrisi->el < pom->el)
-		pom->lijevo = izbrisiElement(pom->lijevo, obrisi);
-
-	else if (obrisi->el > pom->el)
-		pom->desno = izbrisiElement(pom->desno, obrisi);
-
-	else if (pom->lijevo && pom->desno)
-	{
-		temp = findMinElement(pom->desno);
-		pom->el = temp->el;
-		pom->desno = izbrisiElement(pom->desno, pom);
-	}
-
-	else {
-		temp = pom;
-
-		if (pom->lijevo == NULL)
-			pom = pom->desno;
-		else
-			pom = pom->lijevo;
-
-		free(temp);
-	}
-
-	return pom;
-
 }
 
 int Izbornik()
 {
-	printf("-------------------------------------------------\n");
-	printf("1 - Unesi novi element\n");
-	printf("2 - INORDER\n");
-	printf("3 - PREORDER\n");
-	printf("4 - POSTORDER\n");
-	printf("5 - LEVEL ORDER \n");
-	printf("6 - Izbrisi element stabla\n");
+	printf("0.PREKID\n");
+	printf("1.UNOS NOVOG ELEMENTA:\n");
+	printf("2.PREORDER:\n");
+	printf("3.INORDER:\n");
+	printf("4.POSTORDER:\n");
+	printf("5.LEVELORDER:\n");
+	printf("6.TRAZI ELEMENT:\n");
+	printf("7.IZBRISI ELEMENT:\n");
 
-	return 0;
+
+	return EXIT_SUCCESS;
 }
+
+Stablo Izbor(Stablo root)
+{
+	Stablo izbor = NULL;
+	int odabir=0;
+
+	printf("\nUnos:");
+	scanf(" %d",&odabir);
+
+	
+		switch (odabir)
+		{
+		case 0:
+			DealocirajStablo(root);
+			printf("\nUspjesna dealokacija!\n");
+			return NULL;
+
+		case 1:
+			root = noviEl(root, broj());
+			return root;
+		case 2:
+			Preorder(root);
+			return root;
+
+		case 3:
+			Inorder(root);
+			return root;
+
+		case 4:
+			Postorder(root);
+			return root;
+
+		case 5:
+			LevelOrder(root);
+			return root;
+		
+		case 6:
+			izbor = findElement(root, broj());
+			printf("\nELEMENT=%d\n",izbor->el);
+			return root;
+
+		case 7:
+			izbor = findElement(root, broj());
+			DeleteNode(root, izbor);
+			return root;
+
+		default:
+			return NULL;
+		}
+}
+
+int broj()
+{
+	int broj = 0;
+	printf("Unesite broj:");
+	scanf(" %d",&broj);
+
+	return broj;
+}
+
+Stablo noviEl(Stablo root, int broj)
+{
+	Stablo novi = NULL;
+	
+	novi = root;
+
+	if (novi == NULL)
+	{
+		novi = (Stablo)malloc(sizeof(Tree));
+		if (!novi)
+		{
+			printf("Greska alokacije!\n");
+			return -1;
+		}
+		novi->el = broj;
+		novi->L = NULL;
+		novi->D = NULL;
+	}
+
+	else if (broj < novi->el)
+		novi->L = noviEl(novi->L, broj);
+	else if (broj > novi->el)
+		novi->D = noviEl(novi->D,broj);
+
+	return novi;
+}
+
+
+int Inorder(Stablo root)
+{
+	if (root == NULL)
+		return NULL;
+	Inorder(root->L);
+	printf("%d ",root->el);
+	Inorder(root->D);
+
+	return EXIT_SUCCESS;
+}
+
+int Preorder(Stablo root)
+{
+	if (root == NULL)
+		return NULL;
+	printf("%d ",root->el);
+	Preorder(root->L);
+	Preorder(root->D);
+
+	return EXIT_SUCCESS;
+}
+
+int Postorder(Stablo root)
+{
+	if (root == NULL)
+		return NULL;
+	
+	Postorder(root->L);
+	Postorder(root->D);
+	printf("%d ", root->el);
+
+	return EXIT_SUCCESS;
+}
+
+int LevelOrder(Stablo root)
+{
+	int h = Height(root);
+	int i = 0;
+
+	for (i = 1; i <= h; i++)
+	{
+		PrintLevel(root,i);
+	}
+
+	return EXIT_SUCCESS;
+}
+
+int Height(Stablo root)
+{
+	int lheight = 0;
+	int rheight = 0;
+	if (root == NULL)
+		return NULL;
+	else
+	{
+		lheight = Height(root->L);
+		rheight = Height(root->D);
+
+		if (lheight > rheight)
+			return(lheight + 1);
+		else 
+			return (rheight + 1);
+	}
+
+}
+
+int PrintLevel(Stablo root, int level)
+{
+	if (root == NULL)
+		return NULL;
+
+	if (level == 1)
+		printf("%d ",root->el);
+
+	else if (level > 1)
+	{
+		PrintLevel(root->L,level -1);
+		PrintLevel(root->D,level-1);
+
+	}
+
+	return EXIT_SUCCESS;
+}
+
+Stablo findElement(Stablo root, int broj)
+{
+	if (root == NULL)
+		return NULL;
+	if (broj < root->el)
+		return findElement(root->L, broj);
+	else if (broj > root->el)
+		return findElement(root->D,broj);
+
+
+	return root;
+
+}
+
+Stablo DeleteNode(Stablo root, Stablo izbor)
+{
+	Stablo temp = NULL;
+	if (root == NULL)
+		return NULL;
+
+	else if (izbor->el < root->el)
+		root->L = DeleteNode(root->L,izbor);
+	else if (izbor->el > root->el)
+		root->D = DeleteNode(root->D, izbor);
+	else
+	{
+		//1) nema djece
+		if (root->L == NULL && root->D == NULL)
+		{
+			free(root);
+			root = NULL;
+		}
+		//2) jedno dijete
+		else if (root->L == NULL) {
+			temp = root;
+			root = root->D;
+			free(temp);
+		}
+		else if (root->D == NULL)
+		{
+			temp = root;
+			root = root->L;
+			free(temp);
+		}
+		//3)dvoje djece
+		else
+		{
+			temp = FindMin(root->D);
+			root->el = temp->el;
+			root->D = DeleteNode(root->D,temp->el);
+
+		}
+			
+	}
+
+	return root;
+}
+
+Stablo FindMin(Stablo root)
+{
+	while (root->L != NULL)
+		root = root->L;
+
+	return root;
+}
+
+Stablo DealocirajStablo(Stablo root)
+{
+	if (root != NULL)
+	{
+		DealocirajStablo(root->L);
+		DealocirajStablo(root->D);
+		free(root);
+	}
+
+
+	return NULL;
+}
+
